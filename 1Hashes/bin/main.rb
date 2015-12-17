41a60d96ccdbbb7ea6afc71fc20940d82cd30c50
Akinlaja Adebayo <adebayo@dealdey.com>
2015-12-17 15:14:47 +0100

    Fixed the alphabet counter class and called it successfully from main.rb

--- 1Hashes/bin/main.rb
+++ 1Hashes/bin/main.rb
@@ -1,2 +1,20 @@
-class Main
+#!/usr/bin/ruby
+require '../core/alphabet_counter'
+puts 'Welcome! You supply a word and I will count the number of time each character occur'
+print 'Kindly supply the word:      '
+$user_input = gets.chomp
+
+puts
+
+print 'Do you want me to ignore cases(yes|no)?     '
+$user_cases = gets.chomp
+
+puts
+
+$ignore_cases = $user_cases.to_s == 'yes' ? true : false
+
+$alphabet_counter = AlphabetCounter.new
+$resultant = $alphabet_counter.count_alphabet($user_input, $ignore_cases)
+$resultant.each do |key, value|
+  puts "#{key} appears  #{value} time(s)"
 end
--- 1Hashes/core/alphabet_counter.rb
+++ 1Hashes/core/alphabet_counter.rb
@@ -1,5 +1,16 @@
 #!/usr/bin/ruby
 class AlphabetCounter
 
-	
+	def count_alphabet(word = '', ignorecase = true)
+		$refined_word = ''
+		$unique_alphabets = []
+    $counter = Hash.new(0)
+    $refined_word = ignorecase ? word.downcase : word
+    $unique_alphabets = $refined_word.scan(/./).uniq
+    $unique_alphabets.each do |alp|
+      _nos = $refined_word.count(alp)
+      $counter[alp] = _nos
+    end
+    return $counter
+	end
 end
