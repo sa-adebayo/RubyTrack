2ed1630fabcf8cbd629be3e7c58031f9fb7a83e6
Akinlaja Adebayo <adebayo@dealdey.com>
2015-12-17 17:01:05 +0100

    Finished the Regex and main.rb classes

---
+++ Regex/bin/main.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+require '../core/replace_vowel'
+puts 'Welcome! You supply a word and I will replace all occurrences of vowel characters to \'*\'. isn\'t it cool?'
+print 'Kindly supply the word:      '
+_user_input = gets.chomp
+
+puts
+
+$vowel_replacer = ReplaceVowel.new
+puts $vowel_replacer.replace(_user_input)
+
--- Regex/core/replace_vowel.rb
+++ Regex/core/replace_vowel.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 class ReplaceVowel
   def replace(word = '')
-    word.gsub(/a|e|i|o|u/, '*')
+    word.empty? || word =~ /^\s*$/ ? 'You did not supply anything' : word.gsub(/a|e|i|o|u|A|E|I|O|U/, '*')
   end
 end
