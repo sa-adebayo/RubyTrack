57b89691b4e5f2b79d4226be0bee7ab08f85bce7
Akinlaja Adebayo <adebayo@dealdey.com>
2015-12-18 13:53:27 +0100

    Adjusted the Palindrome main.rb

--- Palindrome/bin/main.rb
+++ Palindrome/bin/main.rb
@@ -1,11 +1,19 @@
 #!/usr/bin/ruby
 require '../core/palindrome'
-puts 'Welcome! This is palindrome in action, you need to supply a word and i\'ll check it right away'
-print 'Kindly supply the word:      '
-_user_input = gets.chomp.to_s
-
-puts
-
-$palindrome = Palindrome.new(_user_input)
-puts $palindrome.check
+puts 'Welcome! This is palindrome in action, you need to supply a word and i\'ll check it right away. Supply q or Q to quit'
+quit = true
+while quit do
+  print 'Kindly supply the word:      '
+  _user_input = gets.chomp.to_s
+  puts
+  if _user_input.downcase == 'q'
+    puts 'Thanks for using this palindrome application'
+    quit = false
+  elsif _user_input.length < 3
+    puts 'You did not supply a valid word. I need a word that is more than two characters'
+  else
+    $palindrome = Palindrome.new(_user_input)
+    puts $palindrome.check
+  end
+end
 puts
