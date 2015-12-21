24b0dc6d827807c34ce4b8263bea96a9622c4222
Akinlaja Adebayo <adebayo@dealdey.com>
2015-12-21 12:18:39 +0100

    Done with the Inverse Exercise

---
+++ Inverse/bin/main.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+require '../core/inverse'
+puts 'Welcome! I want to demonstrate how to override the to_s method in Object class, supply the details below'
+print 'Name:             '
+_user_name = gets.chomp
+print 'Gender (M | F):   '
+_user_gender = gets.chomp
+print 'Age:              '
+_user_age = Integer(gets.chomp)
+case _user_gender
+  when 'm', 'M'
+    _gender = 'Male'
+  else
+    _gender = 'Female'
+end
+_inverse = Inverse.new(_user_name, _gender, _user_age)
+puts _inverse.to_s
