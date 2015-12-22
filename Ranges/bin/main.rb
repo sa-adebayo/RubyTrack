82fc5ff3af65b69d569b7722364f0a30b573ed48
Akinlaja Adebayo <adebayo@dealdey.com>
2015-12-22 10:36:41 +0100

    Fixed some code line in the Ranges class to fit properly into the requirements.

---
+++ Ranges/bin/main.rb
@@ -0,0 +1,5 @@
+require '../core/character_counter'
+_character_counter = CharacterCounter.new
+print 'Enter the word to analyse:      '
+_user_input = gets.chomp
+puts _character_counter.count(_user_input)
--- Ranges/core/character_counter.rb
+++ Ranges/core/character_counter.rb
@@ -1,13 +1,10 @@
 class CharacterCounter
   def count(sentence)
     sentence = sentence.to_s
-    _lowercase = sentence.scan(/a-z/)
-    _uppercase = sentence.scan(/A-Z/)
-    _special_characters = 0
-    _digits = sentence.scan(/0-9/)
-
-    sentence.each_char do |c|
-      #sentence
-    end
+    _lowercase = sentence.scan(/[a-z]/)
+    _uppercase = sentence.scan(/[A-Z]/)
+    _special_characters = sentence.scan(/[^[:alnum:][:blank:]]/)
+    _digits = sentence.scan(/[0-9]/)
+    return "Uppercase:          #{_uppercase.length} \nLowercase:          #{_lowercase.length} \nDigits:             #{_digits.length}\nSpecial Characters: #{_special_characters.length}"
   end
 end
