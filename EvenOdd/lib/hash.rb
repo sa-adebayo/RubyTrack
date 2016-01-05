9bdaa05ca5676948bbdda1b16a6e2433cbe836f5
Akinlaja Adebayo <adebayo@dealdey.com>
2016-01-05 15:48:54 +0100

    Adjusted the Hash lib file

--- EvenOdd/lib/hash.rb
+++ EvenOdd/lib/hash.rb
@@ -2,14 +2,13 @@ require_relative '../../Hash/lib/array'
 class Hash
   def sort_inject
     sorted_hash = Hash.new { |hash, key| hash[key] = [] }
-    inject(0) do
-    |map, (key, value)|
+    inject do |map, (key, value)|
       if Integer(key).even?
         sorted_hash[:even] << value
       else
         sorted_hash[:odd] << value
       end
     end
-    print sorted_hash
+    sorted_hash
   end
 end
